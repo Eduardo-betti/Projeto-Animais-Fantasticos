7ff57bb304790ec33bd5a8ec755704ca5bc1e2dd
Eduardo Costa Betti <edubetti@hotmail.com>
2023-06-01 21:06:36 -0300

    Postagem no Repositorio do HTML e CSS

---
+++ exercicios/exercicio-event-01.js
@@ -0,0 +1,17 @@
+// Retorne o url da página atual utilizando o objeto window
+
+const localUrl = window.location.href;
+console.log(localUrl);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+const linguagem = navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
+
+const larguraDaJanela = window.innerWidth;
+console.log(larguraDaJanela);
